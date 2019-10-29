9c0cf77867f810234ca958805dd829b7f520e7bf
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-29 16:45:18 -0400

    creates get to post by id

--- postRouter.js
+++ postRouter.js
@@ -8,6 +8,15 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({ error: "The posts information could not be retrieved." }))
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    db.findById(id)
+    .then(post => {
+        post.length ? res.status(200).json(post) : res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+    .catch(err => res.status(500).json({ error: "The post information could not be retrieved." }))
+})
+
 router.post('/', (req, res) => {
     const newPost = req.body;
     !newPost.title || !newPost.contents ? res.status(400).json({ error: "Please provide title and contents for the post." })
