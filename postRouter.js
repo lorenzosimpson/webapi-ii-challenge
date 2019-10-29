2f4dcef729760d105bb815c86f03b845dba52ebe
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-29 18:40:43 -0400

    cleans up formatting and syntax

--- postRouter.js
+++ postRouter.js
@@ -21,13 +21,13 @@ router.get('/:id/comments', (req, res) => {
     const id = req.params.id;
     db.findById(id)
     .then(post => {
-        if (post.length) {
+        post.length ? 
         db.findPostComments(id)
         .then(comments => res.status(200).json(comments))
         .catch(err => console.log(err))
-        } else {
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
-        }
+        :
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+        
     })
     .catch(err => res.status(500).json({ error: "The comments information could not be retrieved." }))
 
@@ -48,9 +48,7 @@ router.post('/:id/comments', (req, res) => {
     const commentObj = {...req.body, post_id};
 
     !commentObj.text ? res.status(400).json({ errorMessage: "Please provide text for the comment." })
-
     : 
-
     db.findById(id)
     .then(post => {
         post.length ? 
@@ -76,15 +74,13 @@ router.put('/:id', (req, res) => {
         db.update(id, newObj)
         .then(updated => 
             db.findById(id)
-            .then(newPost => res.status(200).json(newPost))
-            ) 
+            .then(newPost => res.status(200).json(newPost))) 
         : res.status(404).json({ message: "The post with the specified ID does not exist." })
     })
     .catch(err => res.status(500).json({ error: "The post information could not be modified." }))
 })
 
 router.delete('/:id', (req, res) => {
-    // db.remove(id) => no. of records deleted
     const id = req.params.id;
     //find post with the given id
     db.findById(id)
