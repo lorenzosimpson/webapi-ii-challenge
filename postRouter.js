e5bc1cfd8f18afa5bef34cb1745b2a499b6d4ef6
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-29 18:14:14 -0400

    completes put request

--- postRouter.js
+++ postRouter.js
@@ -63,4 +63,25 @@ router.post('/:id/comments', (req, res) => {
 })
 
 
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const newObj = req.body;
+    // if no title or contents fields in request body
+    !newObj.title || !newObj.contents ? res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    : 
+    // if both present, move forward
+    db.findById(id)
+    .then(post => {
+        post.length ? 
+        db.update(id, newObj)
+        .then(updated => 
+            db.findById(id)
+            .then(newPost => res.status(200).json(newPost))
+            ) 
+        : res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+    .catch(err => res.status(500).json({ error: "The post information could not be modified." }))
+})
+
+
 module.exports = router;
