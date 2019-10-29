2bfd8ad26ba13ab861a06e154bface54123a288a
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-29 16:35:49 -0400

    adds post request to add new post

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const postRouter = require('./postRouter');
 
 const server = express();
 
+
+server.use(express.json())
+
+
 server.use('/api/posts', postRouter)
 
 server.listen(5000, () => console.log('\n=== Server listening on port 5000 ===\n'))
--- postRouter.js
+++ postRouter.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
 const db = require('./data/db');
 
+
 router.get('/', (req, res) => {
     db.find()
     .then(posts => res.status(200).json(posts))
     .catch(err => res.status(500).json({ error: "The posts information could not be retrieved." }))
 })
+
+router.post('/', (req, res) => {
+    const newPost = req.body;
+    !newPost.title || !newPost.contents ? res.status(400).json({ error: "Please provide title and contents for the post." })
+    :
+    db.insert(newPost)
+    .then(post => res.status(201).json(newPost))
+    .catch(err => res.status(500).json({ error: "There was an error while saving the post to the database" }))
+})
+
 
 module.exports = router;
